dd3ee9a2ca50ada5d299f0f20f9ec6340660bf76
Gareth NORMAN <gareth.norman@protonmail.com>
2023-02-17 18:39:18 +0800

    Fixed closing ressources related to DB
    In TicketDAO

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -1,18 +1,19 @@
 package com.parkit.parkingsystem.dao;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
-import com.parkit.parkingsystem.constants.DBConstants;
-import com.parkit.parkingsystem.constants.ParkingType;
-import com.parkit.parkingsystem.model.ParkingSpot;
-import com.parkit.parkingsystem.model.Ticket;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.Calendar;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.constants.DBConstants;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 
 public class TicketDAO {
 
@@ -20,12 +21,12 @@ public class TicketDAO {
 
     public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-    @SuppressWarnings("finally")
     public boolean saveTicket(Ticket ticket) {
 	Connection con = null;
+	PreparedStatement ps = null;
 	try {
 	    con = dataBaseConfig.getConnection();
-	    PreparedStatement ps = con.prepareStatement(DBConstants.SAVE_TICKET);
+	    ps = con.prepareStatement(DBConstants.SAVE_TICKET);
 	    // ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
 	    // ps.setInt(1,ticket.getId());
 	    ps.setInt(1, ticket.getParkingSpot().getId());
@@ -36,25 +37,29 @@ public class TicketDAO {
 		    (ticket.getOutTime() == null) ? null : (new Timestamp(ticket.getOutTime().getTimeInMillis())));
 	    ps.setBoolean(6, ticket.getIsExistingUser());
 	    return ps.execute();
+
 	} catch (Exception ex) {
 	    logger.error("Error fetching next available slot", ex);
 	} finally {
 	    dataBaseConfig.closeConnection(con);
-	    return false;
+	    dataBaseConfig.closePreparedStatement(ps);
 	}
+	return false;
     }
 
     public Ticket getTicket(String vehicleRegNumber) {
 	Connection con = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
 	Ticket ticket = null;
 	Timestamp inTimeTimeStampQuery;
 	Timestamp outTimeTimeStampQuery;
 	try {
 	    con = dataBaseConfig.getConnection();
-	    PreparedStatement ps = con.prepareStatement(DBConstants.GET_TICKET);
+	    ps = con.prepareStatement(DBConstants.GET_TICKET);
 	    ps.setString(1, vehicleRegNumber);
 	    // ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
-	    ResultSet rs = ps.executeQuery();
+	    rs = ps.executeQuery();
 	    if (rs.next()) {
 		ticket = new Ticket();
 		ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(7)), false);
@@ -72,21 +77,23 @@ public class TicketDAO {
 		ticket.setOutTime(outTime);
 		ticket.setIsExistingUser(rs.getBoolean(6));
 	    }
-	    dataBaseConfig.closeResultSet(rs);
-	    dataBaseConfig.closePreparedStatement(ps);
 	} catch (Exception ex) {
 	    logger.error("Error fetching next available slot", ex);
 	} finally {
+
 	    dataBaseConfig.closeConnection(con);
+	    dataBaseConfig.closePreparedStatement(ps);
+	    dataBaseConfig.closeResultSet(rs);
 	}
 	return ticket;
     }
 
     public boolean updateTicket(Ticket ticket) {
 	Connection con = null;
+	PreparedStatement ps = null;
 	try {
 	    con = dataBaseConfig.getConnection();
-	    PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
+	    ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
 	    ps.setDouble(1, ticket.getPrice());
 	    ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTimeInMillis()));
 	    ps.setInt(3, ticket.getId());
@@ -96,6 +103,7 @@ public class TicketDAO {
 	    logger.error("Error saving ticket info", ex);
 	} finally {
 	    dataBaseConfig.closeConnection(con);
+	    dataBaseConfig.closePreparedStatement(ps);
 	}
 	return false;
     }
